e9d7b663303ee868d7ad652321881f0ed3e16dac
Alvin Dennis <alvindennis80@gmail.com>
2025-09-29 13:28:35 +0530

    fix: image viewing error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: "/Dynamic-web",
   assetPrefix: "/Dynamic-web/",
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
